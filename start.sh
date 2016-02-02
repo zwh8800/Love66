18564f5677d9a9f95f8e60918d73e9ca9f4d020d
Zhang Weihu <496781108@qq.com>
2016-02-02 12:09:27 +0800

    修改start

--- start.sh
+++ start.sh
@@ -1,1 +1,3 @@
-go run main.go 431179 - 2>log | mplayer -rawaudio samplesize=2:channels=2:rate=48000 -demuxer rawaudio -
+#!/bin/sh
+
+go run main.go 156277 - 2>log | mplayer -rawaudio samplesize=2:channels=2:rate=48000 -demuxer rawaudio -
