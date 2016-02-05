33b8c19826cb84ea5341008696eb850b66a5178b
Zhang Weihu <496781108@qq.com>
2016-02-05 18:28:28 +0800

    输出字数限制

--- room/room_test.go
+++ room/room_test.go
@@ -3,7 +3,7 @@ package room
 import "testing"
 
 func TestGetInfo(t *testing.T) {
-	roomIds := []int{156277, 3258, 423574}
+	roomIds := []int{156277, 3258, 423574, 60937}
 	rooms := make([]*DouyuRoom, 0)
 	for _, id := range roomIds {
 		room, err := NewDouyuRoom(id)
@@ -26,7 +26,7 @@ func TestGetInfo(t *testing.T) {
 		t.Logf("\troom name: %s", room.RoomName())
 		t.Logf("\thost nickname: %s", room.Nickname())
 		t.Logf("\tgame name: %s", room.GameName())
-		t.Logf("\tdetail: %s", room.Details()[:16])
+		t.Logf("\tdetail: %s", string([]rune(room.Details())[:10]))
 		t.Logf("\tlive stream url: %s", room.LiveStreamUrl())
 	}
 }
